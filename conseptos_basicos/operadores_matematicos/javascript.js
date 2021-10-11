d3190cd880b24742c5a7ebf618fb8369306ad10a
XxrrxX <xxrrxx3888@gmail.com>
2021-10-11 01:39:31 -0500

    JavaScript

---
+++ conseptos_basicos/operadores_matematicos/javascript.js
@@ -0,0 +1,52 @@
+document.write("Suma: <br>");
+var x =  10 + 5; // Suma dentro de una variable
+document.write(x);
+document.write("<br>");
+var x = 10;
+var y = x + 5 + 22 + 45 + 6548; //Suma anidada y asignada en una variable
+document.write(y);
+document.write("<br>");
+document.write(eval("10*20+9")); //Suma de numeros en una string
+document.write("<br>");
+var test =5+7;
+document.write(test+"<br>");
+//multiplicacion
+document.write("Multiplicacion: <br>");
+var x=10*5; //Multiplicacion asignada dentro de una variable
+document.write(x+"<br>");
+//Divicion
+document.write("Division: <br>");
+var x = 100/5; //Division dentro de una variable
+document.write(x+"<br>");
+//El modulo
+document.write("Operador Modulo: <br>");
+var myVariable = 38%5; //El operador modulo % retorna el resto de la division (lo que sobra)
+document.write(myVariable+"<br>");
+// Incremento ++ y decremento --
+
+document.write("Incremento y decremento: <br>");
+//Post incremento
+document.write("Post incremento: <br>");
+var a = 0; var b = 10;
+document.write(a+" "+b+"<br>");
+var a = b++;
+
+document.write(a +" "+b+"<br>"); //Pre incremento
+document.write("Pre incremento <br>");
+var a = 0; var b = 10;
+document.write(a+" "+b+"<br>");
+var a = ++b;
+document.write(a+" "+b+"<br>");
+//Post decremento
+document.write("Post decremento: <br>");
+var a = 0; var b = 10;
+document.write(a+" "+b+"<br>");
+var a = b--;
+
+document.write(a +" "+b+"<br>"); //Pre decremento
+document.write("Pre decremento <br>");
+var a = 0; var b = 10;
+document.write(a+" "+b+"<br>");
+var a = --b;
+document.write(a+" "+b);
+
